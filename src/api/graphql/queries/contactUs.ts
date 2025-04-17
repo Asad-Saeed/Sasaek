5331cccad1728781ddf19b169a27ab2df2e294d6
MN <mn@MNs-MacBook-Pro.local>
2025-04-17 18:08:59 +0500

    feat:design done

--- src/api/graphql/queries/contactUs.ts
+++
@@ -1,9 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const ContactUs = gql`
-  mutation CreateContactUs($data: ContactUsInput!) {
-    createContactUs(data: $data) {
-      message
-    }
-  }
-`;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from "next";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
 import "./globals.css";
 import localFont from "next/font/local";
-import HeroUIProvider from "@/component/common/provider/heroUIProvider";
 import Footer from "@/component/layout/footer";
 import Header from "@/component/layout/header";
 import { MediaQueriesProvider } from "@/component/common/hooks/mediaQuery";
-import ApploClientProvider from "@/component/common/provider/apploClientProvider";
 
 const pilatExtended = localFont({
   src: [
@@ -30,11 +27,6 @@ const pilatExtended = localFont({
   variable: "--font-pilat",
 });
 
-const client = new ApolloClient({
-  uri: "https://flyby-router-demo.herokuapp.com/",
-  cache: new InMemoryCache(),
-});
-
 export const metadata: Metadata = {
   title: "Gymstar",
   description: "Gymstar",
@@ -49,11 +41,9 @@ export default function RootLayout({
     <html lang="en" className={pilatExtended.variable}>
       <body className={`antialiased bg-[#FFFFFF]`}>
         <MediaQueriesProvider>
-          <HeroUIProvider>
-            <Header />
-            <ApploClientProvider>{children}</ApploClientProvider>
-            <Footer />
-          </HeroUIProvider>
+          <Header />
+          {children}
+          <Footer />
         </MediaQueriesProvider>
       </body>
     </html>
